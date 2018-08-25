fd45aadea1459af230ce4adda5ef0f4b0405bbe0
unknown <matthew@sunriseintegration.com>
2018-08-24 18:27:42 -0700

    Remove axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 Vue.config.productionTip = false;
 
-window.axios = require("axios");
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
 new Vue({
   render: h => h(App)
 }).$mount("#app");
